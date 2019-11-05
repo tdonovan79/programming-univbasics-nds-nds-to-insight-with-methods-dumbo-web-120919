3e709aeb8e8eaaffb4548d2b4d93293802f3c14b
Thomas Donovan <tdonovan79@gmail.com>
2019-11-05 23:21:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,6 +17,21 @@ def gross_for_director(director_data)
   director_total = 0
   #director iterator
   director_element = 0
+  #iterate through directors
+  while director_element < directors_database.length do
+    #movie iterator
+    movie_element = 0
+    #iterate through movies
+    while movie_element < directors_database[director_element][:movies].length do
+      #add movie gross to total for director
+      director_total += directors_database[director_element][:movies][movie_element][:gross]
+      #increase movie iterator
+      movie_element += 1
+    end
+    #increase director iterator
+    director_element += 1
+  end
+  return director_total
 end
 
 
